623d3a56202f872352cbc6aa15348d0e6aaf29b7
Daniel Hürtgen <daniel@higidi.de>
2013-01-10 10:52:13 +0100

    [Init] Init new extension.

---
+++ ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "rs_pagegenlock".
+ *
+ * Auto generated 10-01-2013 10:16
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title'                         => 'RS | Advanced Page Generation Locking',
+	'description'                   => '',
+	'category'                      => 'plugin',
+	'author'                        => 'Daniel Hürtgen, Kai Lehmkühler, Florian in der Beek, Julian Scheidsteger',
+	'author_email'                  => 'huertgen@rheinschafe.de, lehmkuehler@rheinschafe.de, inderbeek@rheinschafe.de, scheidsteger@rheinschafe.de',
+	'author_company'                => 'Rheinschafe GmbH, Rheinschafe GmbH, Rheinschafe GmbH, Rheinschafe GmbH',
+	'shy'                           => '',
+	'priority'                      => '',
+	'module'                        => '',
+	'state'                         => 'alpha',
+	'internal'                      => '',
+	'uploadfolder'                  => 0,
+	'createDirs'                    => '',
+	'modify_tables'                 => '',
+	'clearCacheOnLoad'              => 1,
+	'lockType'                      => '',
+	'version'                       => '0.1.0',
+	'constraints'                   => array(
+		'depends'   => array(
+			'typo3'   => '4.5-0.0.0',
+		),
+		'conflicts' => array(),
+		'suggests'  => array(),
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
