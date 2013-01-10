c96089fd3fd761f684caea4841a36ed22e631f3e
Daniel Hürtgen <daniel@higidi.de>
2013-01-10 11:15:12 +0100

    [Locker] Implements init xclass implementation setup (without real logic).

---
+++ ext_localconf.php
@@ -0,0 +1,43 @@
+<?php
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Daniel Hürtgen <huertgen@rheinschafe.de>, Rheinschafe GmbH
+ *  Kai Lehmkühler <lehmkuehler@rheinschafe.de>, Rheinschafe GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Configuration of the rs_pagegenlock package.
+ *
+ * @package    rs_pagegenlock
+ * @license    http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+
+// access restriction
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+// X-Class implementation for t3lib_lock class (ux_t3lib_lock)
+$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_lock.php'] = t3lib_extMgm::extPath(
+	$_EXTKEY, 'Classes/Legacy/class.ux_t3lib_lock.php'
+);
