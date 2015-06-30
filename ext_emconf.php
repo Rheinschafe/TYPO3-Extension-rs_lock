7aca42cc101f5dd2a25925e3b70312cd02644a16
Daniel Hürtgen <daniel@higidi.com>
2015-06-30 14:49:22 +0200

    [FEATURE] required typo3 support raised to 6.0.0 up to 6.2.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints'                   => array(
 		'depends'   => array(
 			'php'   => '5.3.0-0.0.0',
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '6.0.0-6.2.99',
 		),
 		'conflicts' => array(),
 		'suggests'  => array(),
