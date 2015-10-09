027f504904bf0fd7782b54f8134ccf9129f060fc
Daniel Hürtgen <daniel@higidi.com>
2015-10-09 15:00:35 +0200

    [TASK] remove lock logging TYPO3_CONF_VARS

--- Classes/Locking/Locker.php
+++ Classes/Locking/Locker.php
@@ -74,12 +74,6 @@ class Locker extends CoreLocker {
 	 * @throws \InvalidArgumentException
 	 */
 	public function __construct($id, $method = NULL, $loops = 0, $step = 0) {
-		if ( isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['enableLockLogging'] ) ){
-			$this->setEnableLogging( (boolean)$GLOBALS['TYPO3_CONF_VARS']['SYS']['enableLockLogging'] );
-		}else {
-			$this->setEnableLogging( FALSE );
-		}
-
 		$this->lockFactory = GeneralUtility::makeInstance("Rheinschafe\\RsLock\\Locking\\LockFactory");
 
 		// Force ID to be string
